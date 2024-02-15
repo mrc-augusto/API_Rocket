1039f13ec9990a1a7139d6f166b2e252eecbc8ae
mrc-augusto <mrc_augusto@hotmail.com>
2024-02-15 18:53:05 -0300

    database created

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,22 @@
+const {hash} = require('bcryptjs')
 const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController{
-  create(req, res){
+  async create(req, res){
     const {name, email, password} = req.body
-    if(!name){
-      throw new AppError('Nome é obrigatório')
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(checkUserExists){
+      throw new AppError('Este email já está em uso.')
     }
 
-    res.status(201).json({name, email, password})
+    const hashedPassword = await hash(password, 8)
+
+    await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword])
+
+    return res.status(201).json()
   }
 }
 
